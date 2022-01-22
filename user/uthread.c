aaf2f2ef3345e172b2240f88a4fd5f7203fd20ef
flyFatSeal <491927227@qq.com>
2022-01-22 20:53:25 +0800

    lab uthread

--- user/uthread.c
+++ user/uthread.c
@@ -2,6 +2,8 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+
+
 /* Possible states of a thread: */
 #define FREE        0x0
 #define RUNNING     0x1
@@ -10,15 +12,37 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved registers for kernel context switches.
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
+
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  int        num;
+  struct     context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context*, struct context*);
               
 void 
 thread_init(void)
@@ -63,12 +87,14 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
-  } else
+    thread_switch(&t->context, &next_thread->context);
+  }
+  else
     next_thread = 0;
 }
 
 void 
-thread_create(void (*func)())
+thread_create(void (*func)(),int num)
 {
   struct thread *t;
 
@@ -77,6 +103,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->num = num;
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)t->stack + STACK_SIZE;
 }
 
 void 
@@ -155,9 +184,9 @@ main(int argc, char *argv[])
   a_started = b_started = c_started = 0;
   a_n = b_n = c_n = 0;
   thread_init();
-  thread_create(thread_a);
-  thread_create(thread_b);
-  thread_create(thread_c);
+  thread_create(thread_a,1);
+  thread_create(thread_b,2);
+  thread_create(thread_c,3);
   thread_schedule();
   exit(0);
 }
